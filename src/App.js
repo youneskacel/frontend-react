90ce1b0df73e3020e4d2eb7b6e601b64318197d3
youneskacel <ynskacel@gmail.com>
2024-08-17 16:58:03 +0100

    frontend changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./views/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import CarForm from "./components/CarForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<CarForm />} />
+      <Route path="/:id" element={<CarForm isUpdating />} />
+            
+      </Routes>
+      </Router>
     </div>
   );
 }
